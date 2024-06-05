6833c3e335c77d9fc9a837c0c00cbaea7f206b03
blackflame576 <pws15548@gmail.com>
2024-06-05 22:18:17 +0300

    Refactoring

--- src/test/DatabaseTest.hpp
+++ src/test/DatabaseTest.hpp
@@ -16,6 +16,7 @@ protected:
     std::string macOS_Command;
     std::string Linux_Command;
     std::string Table;
+    std::string DevelopmentTable;
 
     void SetUp() override
     {
@@ -26,6 +27,7 @@ protected:
         macOS_Command = "Test_macOS_Command";
         Linux_Command = "Test_Linux_Command";
         Table = "Test";
+        DevelopmentTable = "DevelopmentPacks";
         if (!std::filesystem::exists(DatabasePath))
         {
             std::ofstream file(DatabasePath);
